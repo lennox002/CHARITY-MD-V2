9b5e260548e1037559ae70106ba2ee974b7fe5bf
caseyweb <caseywebsthedonn25@gmail.com>
2025-01-08 01:25:22 +0300

    𝒄𝒂𝒔𝒆𝒚𝒓𝒉𝒐𝒅𝒆𝒔

--- musicbot_cmds/menu.js
+++ musicbot_cmds/menu.js
@@ -1,96 +1,135 @@
 const util = require('util');
 const fs = require('fs-extra');
-const { zokou } = require(__dirname + "/../framework/zokou");
+const axios = require('axios');
+const { adams } = require(__dirname  + "/../framework/zokou");
 const { format } = require(__dirname + "/../framework/mesfonctions");
 const os = require("os");
 const moment = require("moment-timezone");
-const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
+const s = require(__dirname  + "/../set");
 
-zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
+
+const runtime = function (seconds) { 
+    seconds = Number(seconds); 
+    var d = Math.floor(seconds / (3600 * 24)); 
+    var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+    var m = Math.floor((seconds % 3600) / 60); 
+    var s = Math.floor(seconds % 60); 
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+    return dDisplay + hDisplay + mDisplay + sDisplay; 
+};
+
+// Function to fetch GitHub repo data
+const fetchGitHubStats = async () => {
+    try {
+        const repo = 'caseyweb/ZHEZHO-MD'; // Replace with your repo
+        const response = await axios.get(`https://api.github.com/repos/${repo}`);
+        const forks = response.data.forks_count;
+        const stars = response.data.stargazers_count;
+        const totalUsers = (forks * 2) + (stars * 2);
+        return { forks, stars, totalUsers };
+    } catch (error) {
+        console.error("Error fetching GitHub stats:", error);
+        return { forks: 0, stars: 0, totalUsers: 0 }; 
     }
+};
 
+adams({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage } = commandeOptions;
+    let { cm } = require(__dirname  + "/../framework/zokou");
+    var coms = {};
+    var mode = s.MODE.toLowerCase() === "public" ? "public" : "private";
 
-    
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
+    cm.map((com) => {
+        const categoryUpper = com.categorie.toUpperCase();
+        if (!coms[categoryUpper]) coms[categoryUpper] = [];
+        coms[categoryUpper].push(com.nomCom);
     });
 
-    moment.tz.setDefault('Etc/GMT');
+    moment.tz.setDefault(`${s.TZ}`);
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+    const hour = moment().hour();
 
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
+    let greeting = "Good night";
+    if (hour >= 0 && hour <= 11) greeting = "Good morning";
+    else if (hour >= 12 && hour <= 16) greeting = "Good afternoon";
+    else if (hour >= 16 && hour <= 21) greeting = "Good evening";
 
-  let infoMsg =  `
-> *CHARITY XMD AVAILABLE MENUS* 
-╭─────────────────
-│❍╭─────────────
-│❍│▸ *MENU* 
-│❍│▸ *MENU2* 
-│❍│▸ *HACHERS HOOD*
-│❍╰──────────────
-│❍│▸ *PLUGINS* : ${cm.length} 
-│❍│▸ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-│❍│▸ *SAVER* : ${os.platform()}
-│❍│▸ *THEME* : *CHARITY XMD THEMES*
-│❍╰──────────────
-╰──────────────────\n`;
-    
-let menuMsg = `
+    const { totalUsers } = await fetchGitHubStats();
+    const formattedTotalUsers = totalUsers.toLocaleString();
 
- *𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝚃𝙴𝙲𝙷🍀*${readmore}
-`;
+    let infoMsg = `
+╭─────═━┈┈━═──━┈⊷
+┇ ʙᴏᴛ ɴᴀᴍᴇ: *ʙᴍᴡ ᴍᴅ*
+┇ ᴍᴏᴅᴇ: *${mode}*
+┇ ᴘʀᴇғɪx: *[ ${prefixe} ]*
+┇ ᴘʟᴀᴛғᴏʀᴍ: *${os.platform()}*
+┇ ᴛʏᴘᴇ: *ᴠ6x*
+┇ ᴅᴀᴛᴇ: *${date}*
+┇ ᴛɪᴍᴇ: *${temps}*
+┇ ᴄᴀᴘᴀᴄɪᴛʏ ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+╰─────═━┈┈━═──━┈⊷\n\n
+🌍 𝐁𝐄𝐒𝐓 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓 🌍\n\n`;
 
-    for (const cat in coms) {
-        menuMsg += ` ╭────────❒⁠⁠⁠⁠ *${cat}* ✣`;
-        for (const cmd of coms[cat]) {
-            menuMsg += `
-│❍│▸ ${cmd}`;
-        }
-        menuMsg += `
-╰────────────···▸▸ \n`
-    }
+    let menuMsg = `${readmore}  
+╭─────═━┈┈━═──━┈⊷
+┇ ʙᴍᴡ ᴍᴅ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ
+╰─────═━┈┈━═──━┈⊷\n\n`;
 
-    menuMsg += `> 𝙼𝙰𝙳𝙴 𝙴𝙰𝚂𝚈 𝙱𝚈 𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂🍀
-`;
+    const sortedCategories = Object.keys(coms).sort();
+    sortedCategories.forEach((cat) => {
+        menuMsg += `*╭────❒* *${cat}* *❒*`;
+        coms[cat].forEach((cmd) => {
+            menuMsg += `\n*╏* ${cmd}`;
+        });
+        menuMsg += `\n*╰─═════════════❒*\n`;
+    });
 
-   var lien = mybotpic();
+    menuMsg += `
+▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
+©CASEYRHODES PROJECT 
+▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄`;
 
-   if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *msela-chui-v2*, déveloper mselachui Tech" , gifPlayback : true }, { quoted: ms });
-    }
-    catch (e) {
+        await zk.sendMessage(dest, { 
+            text: infoMsg + menuMsg,
+            contextInfo: {
+                mentionedJid: [nomAuteurMessage],
+                externalAdReply: {
+                    body: "© Caseyrhodes",
+                    thumbnailUrl: "https://files.catbox.moe/ytix9f.jpeg",
+                    sourceUrl: 'https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E',
+                    mediaType: 1,
+                   renderLargerThumbnail: true
+                }
+            }
+        });
+
+        // Send audio with caption
+        await zk.sendMessage(dest, { 
+            audio: { 
+                url: "https://files.catbox.moe/oordg5.mp3" // Replace with your audio URL
+            }, 
+            mimetype: 'audio/mp4', 
+            ptt: false, // Set to true if you want it as a voice note
+            caption: "BMW MD SONG",
+            contextInfo: {
+                externalAdReply: {
+                    body: "BMW SONG BY IBRAHIM",
+                    thumbnailUrl: "https://files.catbox.moe/va22vq.jpeg",
+                    sourceUrl: 'https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E',
+                    rendersmallThumbnail: false
+                }
+            }
+        });
+
+    } catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
         repondre("🥵🥵 Menu erreur " + e);
     }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *msela-chui-v2*, déveloper mselachui Tech" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
 });
