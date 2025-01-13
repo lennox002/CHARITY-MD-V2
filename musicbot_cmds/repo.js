b937d73d03a091f0fb52a44c59b51144e4a122fb
caseyweb <caseywebsthedonn25@gmail.com>
2025-01-14 02:39:31 +0300

    Update repo.js

--- musicbot_cmds/repo.js
+++ musicbot_cmds/repo.js
@@ -2,83 +2,58 @@ const util = require('util');
 const fs = require('fs-extra');
 const { zokou } = require(__dirname + "/../framework/zokou");
 const { format } = require(__dirname + "/../framework/mesfonctions");
-const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
 
 zokou({ nomCom: "repo", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
-    }
+    let { ms, repondre, nomAuteurMessage } = commandeOptions;
 
-
-    
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
-    });
-
+    // Set timezone to GMT
     moment.tz.setDefault('Etc/GMT');
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
 
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-
-  let infoMsg =  `
+    // Message content
+    let infoMsg = `
 *AVAILABLE REPO AND GROUPS* 
-╭─────────────────
-│❍╭─────────────
-│❍│▸ *CHANNEL* 
-│❍⁠⁠⁠⁠│▸ *GROUP* 
-│❍│▸ *REPO*
-│❍⁠⁠⁠⁠╰──────────────
-│❍│▸ *CHANNEL* : https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E
-│❍│▸ *GROUP* : https://chat.whatsapp.com/Lcw1jJCMa6a82RDEW5XM1j
-│❍│▸ *REPO* : https://github.com/caseyweb/CHARITY-MD-V2
-│❍⁠⁠⁠⁠│▸ *YTUBE* : https://www.youtube.com/@Caseyrhodes01
-│❍⁠⁠⁠⁠╰──────────────
-╰──────────────────\n
-  `;
-    
-let menuMsg = `
-     MADE EASY BY CASEYRHODES 🍀
+*hellow Friend👋👋*
+this is* *CHARITY-XMD👊.*
 
+🗼 *REPOSITORY:* https://github.com/caseyweb/CHARITY-MD-V2
+💫 *STARS:* 20
+🧧 *FORKS:* 63
+📅 *RELEASE DATE:* 08/01/2025
+🕐 *UPDATE ON:* 2025-01-13T21:36:23Z
+🙊 *OWNER:* *Mr Caseyrhodes*
+🍃 *THEME:* *CHARITY-XMD*
+🍷 *Am Safe To Fight In My Life*
+╭─────────────────
+│❍│▸ *REPO* : https://github.com/caseyweb/CHARITY-MD-V2
+│❍│▸ *YTUBE* : https://www.youtube.com/@Caseyrhodes01
+╰──────────────────
+    `;
+
+    let menuMsg = `
+  MADE EASY BY CASEYRHODES🔱
 ❍────────────────────❍`;
 
-   var lien = mybotpic();
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+        // Send message without using any bot image
+        await zk.sendMessage(dest, {
+            text: infoMsg + menuMsg,
+            contextInfo: {
+                mentionedJid: [nomAuteurMessage],
+                externalAdReply: {
+                    title: "𝐂𝐇𝐀𝐑𝐈𝐓𝐘 𝐗𝐌𝐃 WHATSAPP BOT",
+                    body: "MADE BY 𝗖𝗔𝗦𝗘𝗬𝗥𝗛𝗢𝗗𝗘𝗦",
+                    thumbnailUrl: "https://files.catbox.moe/81hhl0.jpg", // Static bot image URL
+                    sourceUrl: "https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E",
+                    mediaType: 1
+                }
+            }
+        }, { quoted: ms });
+    } catch (error) {
+        console.error("❌ Error sending GitHub info:", error);
+        repondre("❌ Error sending GitHub info: " + error.message);
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
-}); 
+});
